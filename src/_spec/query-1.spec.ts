4f5b4390ec3d71abd86b660ccd84ce98348073a7
shellyln <shellyln@users.noreply.github.com>
2020-08-07 11:06:40 +0900

    [FIX] Fix CSV parser

--- src/_spec/query-1.spec.ts
+++ src/_spec/query-1.spec.ts
@@ -18,21 +18,38 @@ describe("query-1", function() {
                 query: {
                     Contact: staticCsvResolverBuilder(
                         'Contact', () => Promise.resolve(`
-                            Id,         Foo,    Bar,    Baz,    Qux,    Quux,   AccountId
-                            Contact/z1, aaa/z1, bbb/z1, ccc/z1, ddd/z1, eee/z1, Account/z1
-                            Contact/z2, aaa/z2, bbb/z2, ccc/z2, ddd/z2, eee/z2, Account/z1
-                            Contact/z3, aaa/z3, bbb/z3, ccc/z3, ddd/z3, eee/z3, Account/z2
+                            Id         , Foo      , Bar      , Baz      , Qux      , Quux     ,   Corge , Grault       , Garply                , AccountId
+                            Contact/z1 , aaa/z1   , bbb/z1   , ccc/z1   , ddd/z1   , eee/z1   ,    -1.0 , 2019-12-31   , 2019-12-31T23:59:59   , Account/z1
+                            Contact/z2 , aaa/z2   , bbb/z2   , ccc/z2   , ddd/z2   , eee/z2   ,     0.0 , 2020-01-01   , 2020-01-01T00:00:00   , Account/z1
+                            Contact/z3 , "aaa/z3" , "bbb/z3" , "ccc/z3" , "ddd/z3" , "eee/z3" ,     1   , "2020-01-02" , "2020-01-01T00:00:01" , "Account/z2"
+                            Contact/z4 ,          ,          ,          ,          ,          ,         ,              ,                       ,
+                            Contact/z5 ,       "" ,       "" ,      " " ,       "" ,       "" ,         ,              ,                       ,
                         `)
                     ),
                 },
             },
         });
-        const result = await soql`select id, foo, bar, baz from contact`;
-        const expects = [
-            { Id: 'Contact/z1', Foo: 'aaa/z1', Bar: 'bbb/z1', Baz: 'ccc/z1' },
-            { Id: 'Contact/z2', Foo: 'aaa/z2', Bar: 'bbb/z2', Baz: 'ccc/z2' },
-            { Id: 'Contact/z3', Foo: 'aaa/z3', Bar: 'bbb/z3', Baz: 'ccc/z3' },
-        ];
-        expect(result).toEqual(expects);
+        {
+            const result = await soql`select id, foo, bar, baz from contact`;
+            const expects = [
+                { Id: 'Contact/z1', Foo: 'aaa/z1', Bar: 'bbb/z1', Baz: 'ccc/z1' },
+                { Id: 'Contact/z2', Foo: 'aaa/z2', Bar: 'bbb/z2', Baz: 'ccc/z2' },
+                { Id: 'Contact/z3', Foo: 'aaa/z3', Bar: 'bbb/z3', Baz: 'ccc/z3' },
+                { Id: 'Contact/z4', Foo:     null, Bar:     null, Baz:     null },
+                { Id: 'Contact/z5', Foo:       '', Bar:       '', Baz:      ' ' },
+            ];
+            expect(result).toEqual(expects);
+        }
+        {
+            const result = await soql`select id, corge, grault, garply from contact`;
+            const expects = [
+                { Id: 'Contact/z1', Corge:   -1, Grault: '2019-12-31', Garply: '2019-12-31T23:59:59' },
+                { Id: 'Contact/z2', Corge:    0, Grault: '2020-01-01', Garply: '2020-01-01T00:00:00' },
+                { Id: 'Contact/z3', Corge:    1, Grault: '2020-01-02', Garply: '2020-01-01T00:00:01' },
+                { Id: 'Contact/z4', Corge: null, Grault:         null, Garply:                  null },
+                { Id: 'Contact/z5', Corge: null, Grault:         null, Garply:                  null },
+            ];
+            expect(result).toEqual(expects);
+        }
     });
 });
