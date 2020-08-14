ef745b8f218b4818e5c1980406d29db2e78221e7
shellyln <shellyln@users.noreply.github.com>
2020-08-14 23:29:09 +0900

    [FIX] Fix CSV parser: parse boolean values

--- src/_spec/functions-1.spec.ts
+++ src/_spec/functions-1.spec.ts
@@ -17,6 +17,136 @@ import { resolverConfigs }            from './helpers/config';
 
 
 
+const commands1 = build({
+    functions: [{
+        type: 'scalar',
+        name: 'testspec_string_concat',
+        fn: (ctx, args, records) => {
+            return args.map(c => String(c)).join('');
+        },
+    }, {
+        type: 'scalar',
+        name: 'testspec_number_add',
+        fn: (ctx, args, records) => {
+            return args.map(c => Number(c)).reduce((a, b) => a + b);
+        },
+    }, {
+        type: 'scalar',
+        name: 'testspec_twice',
+        fn: (ctx, args, records) => {
+            return Number(args[0]) * 2;
+        },
+    }, {
+        type: 'immediate-scalar',
+        name: 'testspec_pass_thru',
+        fn: (ctx, args) => {
+            return args[0];
+        },
+    }],
+    relationships: {
+        Account: {
+            Contacts: ['Contact'],
+            // Contacts: ['Contact', 'Account'],
+            Opportunities: ['Opportunity'],
+            // Opportunities: ['Opportunity', 'Account'],
+        },
+        Contact: {
+            Account: 'Account',
+            // Account: { resolver: 'Account', id: 'AccountId' },
+        },
+        Opportunity: {
+            Account: 'Account',
+            // Account: { resolver: 'Account', id: 'AccountId' },
+        },
+        Event: {
+            Account: { resolver: 'Account', id: 'WhatId' },
+            Contact: { resolver: 'Contact', id: 'WhatId' },
+            Opportunity: { resolver: 'Opportunity', id: 'WhatId' },
+        },
+    },
+    resolvers: {
+        query: {
+            Contact: staticCsvResolverBuilder(
+                'Contact', () => Promise.resolve(`
+                    Id         , Foo      , Bar      , Baz      , Qux      , Quux     ,   Corge , Grault       , Garply                 , AccountId
+                    Contact/z1 , aaa/z1   , bbb/z1   , ccc/z1   , ddd/z1   , eee/z1   ,    -1.0 , 2019-12-31   , 2019-12-31T23:59:59Z   , Account/z1
+                    Contact/z2 , aaa/z2   , bbb/z2   , ccc/z2   , ddd/z2   , eee/z2   ,     0.0 , 2020-01-01   , 2020-01-01T00:00:00Z   , Account/z1
+                    Contact/z3 , "aaa/z3" , "bbb/z3" , "ccc/z3" , "ddd/z3" , "eee/z3" ,     1   , "2020-01-02" , "2020-01-01T00:00:01Z" , "Account/z2"
+                    Contact/z4 ,          ,          ,          ,          ,          ,         ,              ,                        ,
+                    Contact/z5 ,       "" ,       "" ,      " " ,       "" ,       "" ,         ,              ,                        ,
+                `)
+            ),
+            Account: staticCsvResolverBuilder(
+                'Account', () => Promise.resolve(`
+                    Id         , Name     , Address
+                    Account/z1 , fff/z1   , ggg/z1
+                    Account/z2 , fff/z2   , ggg/z2
+                    Account/z3 , "fff/z3" , "ggg/z3"
+                    Account/z4 ,          ,
+                    Account/z5 ,       "" ,       ""
+                `)
+            ),
+            Opportunity: staticCsvResolverBuilder(
+                'Opportunity', () => Promise.resolve(`
+                    Id             , Name     , Amount , AccountId
+                    Opportunity/z1 , hhh/z1   ,   1000 , Account/z1
+                    Opportunity/z2 , hhh/z2   ,   2000 , Account/z1
+                    Opportunity/z3 , "hhh/z3" ,   3000 , Account/z2
+                    Opportunity/z4 ,          ,        ,
+                    Opportunity/z5 , ""       ,      0 , Account/z2
+                `)
+            ),
+            Event: staticCsvResolverBuilder(
+                'Event', () => Promise.resolve(`
+                    Id         , Title    , Address  , WhatId
+                    Event/z1   , iii/z1   , jjj/z1   , Account/z2
+                    Event/z2   , iii/z2   , jjj/z2   , Contact/z2
+                    Event/z3   , "iii/z3" , "jjj/z3" , Contact/z3
+                    Event/z4   ,          ,          ,
+                    Event/z5   ,       "" ,       "" , Opportunity/z5
+                `)
+            ),
+        },
+    },
+});
+
+
 describe("functions-1", function() {
-    //
+    // it("Functions (1)", async function() {
+    //     for (const cf of resolverConfigs) {
+    //         setDefaultStaticResolverConfig(cf);
+
+    //         const { soql, insert, update, remove, transaction } = commands1;
+
+    //         {
+    //             const result = await soql`
+    //                 select
+    //                     calendar_month(),
+    //                     calendar_month_lc(),
+    //                     calendar_quarter(),
+    //                     calendar_quarter_lc(),
+    //                     calendar_year(),
+    //                     calendar_year_lc(),
+    //                     day_in_month(),
+    //                     day_in_month_lc(),
+    //                     day_in_week(),
+    //                     day_in_week_lc(),
+    //                     day_in_year(),
+    //                     day_in_year_lc(),
+    //                     day_only(),
+    //                     day_only_lc(),
+    //                     hour_in_day(),
+    //                     hour_in_day_lc(),
+    //                     week_in_month(),
+    //                     week_in_month_lc(),
+    //                     week_in_year(),
+    //                     week_in_year_lc()
+    //                 from contact`;
+    //             const expects = [
+    //                 {},
+    //             ];
+    //             expect(result).toEqual(expects);
+    //         }
+    //     }
+    // });
 });
