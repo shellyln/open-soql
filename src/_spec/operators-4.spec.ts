dace6acd49c9fb0aaf3733a95544457e9b95216f
shellyln <shellyln@users.noreply.github.com>
2020-08-15 19:45:02 +0900

    [WIP] Add spec codes (operators)

--- src/_spec/operators-4.spec.ts
+++ src/_spec/operators-4.spec.ts
@@ -345,6 +345,36 @@ describe("operators-3", function() {
                 ];
                 expect(result).toEqual(expects);
             }
+            {
+                const result = await soql`
+                    select
+                        foo
+                    from contact
+                    where (foo <= 'aaa/z3' and foo<='aaa/z2' and foo<='aaa/z1')`;
+                const expects = [
+                    { Foo: 'aaa/z1' },
+                    // { Foo: 'aaa/z2' },
+                    // { Foo: 'aaa/z3' },
+                    // { Foo: null },
+                    { Foo: '' },
+                ];
+                expect(result).toEqual(expects);
+            }
+            {
+                const result = await soql`
+                    select
+                        foo
+                    from contact
+                    where ((foo <= 'aaa/z3') and (foo<='aaa/z2') and (foo<='aaa/z1'))`;
+                const expects = [
+                    { Foo: 'aaa/z1' },
+                    // { Foo: 'aaa/z2' },
+                    // { Foo: 'aaa/z3' },
+                    // { Foo: null },
+                    { Foo: '' },
+                ];
+                expect(result).toEqual(expects);
+            }
         }
     });
 
@@ -413,6 +443,52 @@ describe("operators-3", function() {
                         { Foo: 'aaa/z3' },
                         // { Foo: null },
                         { Foo: '' },
+                    ];
+                    expect(result).toEqual(expects);
+                }
+
+                {
+                    const result = await soql`
+                        select
+                            foo
+                        from contact
+                        where foo='aaa/z1' or foo='aaa/z2' or foo=null`;
+                    const expects = [
+                        { Foo: 'aaa/z1' },
+                        { Foo: 'aaa/z2' },
+                        // { Foo: 'aaa/z3' },
+                        { Foo: null },
+                        // { Foo: '' },
+                    ];
+                    expect(result).toEqual(expects);
+                }
+                {
+                    const result = await soql`
+                        select
+                            foo
+                        from contact
+                        where (foo='aaa/z1' or foo='aaa/z2' or foo=null)`;
+                    const expects = [
+                        { Foo: 'aaa/z1' },
+                        { Foo: 'aaa/z2' },
+                        // { Foo: 'aaa/z3' },
+                        { Foo: null },
+                        // { Foo: '' },
+                    ];
+                    expect(result).toEqual(expects);
+                }
+                {
+                    const result = await soql`
+                        select
+                            foo
+                        from contact
+                        where ((foo='aaa/z1') or (foo='aaa/z2') or (foo=null))`;
+                    const expects = [
+                        { Foo: 'aaa/z1' },
+                        { Foo: 'aaa/z2' },
+                        // { Foo: 'aaa/z3' },
+                        { Foo: null },
+                        // { Foo: '' },
                     ];
                     expect(result).toEqual(expects);
                 }
